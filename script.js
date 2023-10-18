014595b22e87c43da7d524df1697452a3a3fbc96
Jason Vang <jvang@Jasons-MacBook-Air.local>
2023-10-18 11:22:09 -0500

    Updated solution

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
 $(document).ready(function(){
+    //store the URL for the API with my API key
     var URL = "https://static.codehs.com/api/12345/movies/all"; 
+    //Create click funtion that calls findReleaseDate
     $("#search").click(function(){
-        var movies = $("#movie").val();
+        var movie = $("#movie").val();
         findReleaseDate (movie);
-    });        
-        function findReleaseDate(movie){
-            $.getJSON(URL,handleData)
-            function handleData(data){
-                const movieData = data.find(item=>item.title == movie);
-                if (movieData){
-                    const releaseYear = movieData.release-date;
-                    $("#release-date").text(releaseYear);
-                }else {
-                    $("#release-date").text("Movie not found.");
-                }                    
+    });   
+         
+    function findReleaseDate(movie){
+        $.getJSON(URL,function(data){
+            var releaseDate = "Release date not found.";
+    
+            for(var key in data){
+                if(data[key].name === movie){
+                    releaseDate = data[key]["release-date"];
+                    break;  // Found the movie, exit the loop
+                }
+            }
+    
+            $("#release-date").text("Release Date: " + releaseDate);
+            
 
-            }        
-        }
+        });
+    };
 });
